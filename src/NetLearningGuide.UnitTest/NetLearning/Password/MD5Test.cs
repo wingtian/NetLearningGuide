3de95ed1895096fe37f0d3af911ec228ff68d3b8
Vinson <305896628@qq.com>
2021-06-19 20:01:47 -0700

    MD5 加密测试预告

---
+++ src/NetLearningGuide.UnitTest/NetLearning/Password/MD5Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.Password
+{
+    public class MD5Test
+    {
+        [Fact]
+        public Task Md5UnitTest()
+        {
+            var md5 = new MD5CryptoServiceProvider();
+            var hash = md5.ComputeHash(Encoding.GetEncoding("ABC").GetBytes("EED"));
+           return Task.CompletedTask;
+        }
+
+        private string EncryptionMD5()
+        {
+            return "";
+        }
+
+        private string DecryptionMD5()
+        {
+            return "";
+        }
+    }
+}
