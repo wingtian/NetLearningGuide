ad28ff0f147e2b391dd24f4b5ef898c295620783
Vinson <305896628@qq.com>
2021-07-13 01:30:31 -0700

    GroupByTestCase2

--- src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/LinqTest.cs
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/LinqTest.cs
@@ -38,7 +38,7 @@ namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
                 new LinqModel(){Name = "James",Age = 1,CreateDate = DateTime.Today},
                 new LinqModel(){Name = "Gluee",Age = 1,CreateDate = DateTime.Today},
             };
-            list = list.DistinctBy(p=> new { p.Name,p.Age,p.CreateDate}).ToList();
+            list = list.DistinctBy(p => new { p.Name, p.Age, p.CreateDate }).ToList();
             list.Count.ShouldBe(4);
             return Task.CompletedTask;
         }
@@ -54,7 +54,7 @@ namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
                 new LinqModel(){Name = "James",Age = 1,CreateDate = DateTime.Today},
                 new LinqModel(){Name = "Gluee",Age = 1,CreateDate = DateTime.Today},
             };
-            list = list.GroupBy(p => new { p.Name, p.Age, p.CreateDate }).Select(x=>x.FirstOrDefault()).ToList();
+            list = list.GroupBy(p => new { p.Name, p.Age, p.CreateDate }).Select(x => x.FirstOrDefault()).ToList();
             list.Count.ShouldBe(4);
             return Task.CompletedTask;
         }
@@ -63,6 +63,22 @@ namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
             public string Name { get; set; }
             public int Age { get; set; }
             public DateTime CreateDate { get; set; }
+        }
+        [Fact]
+        public Task GroupByTestCase2()
+        {
+            var list = new List<LinqModel>()
+            {
+                new LinqModel(){Name = "James",Age = 1,CreateDate = DateTime.Today},
+                new LinqModel(){Name = "James",Age = 2,CreateDate = DateTime.Today},
+                new LinqModel(){Name = "Gluee",Age = 1,CreateDate = DateTime.Today},
+                new LinqModel(){Name = "Gluee",Age = 2,CreateDate = DateTime.Today},
+                new LinqModel(){Name = "James",Age = 1,CreateDate = DateTime.Today},
+                new LinqModel(){Name = "Gluee",Age = 1,CreateDate = DateTime.Today},
+            };
+            list = list.GroupBy(p => new LinqModel { Name = p.Name, Age = p.Age, CreateDate = p.CreateDate }).Select(x => x.FirstOrDefault()).ToList();
+            list.Count.ShouldBe(6);
+            return Task.CompletedTask;
         }
     }
 }
