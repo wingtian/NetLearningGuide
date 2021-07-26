7372c1501620d7bb5d5d8bb5c83e026a4761f3a2
Vinson <305896628@qq.com>
2021-07-26 00:07:00 -0700

    Params Test case

---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/ParamsTest.cs
@@ -0,0 +1,43 @@
+﻿using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class ParamsTest
+    {
+        /// https://www.cnblogs.com/maowp/p/8134342.html
+        [Fact]
+        public Task ParamsTestCase1()
+        {
+            var input = new[] { 6, 5, 2, 7, 10, 20, 60, 4 };
+            var result = ShowParamsMaxValue(2, 3);
+            result.ShouldBe(3);
+            result = ShowMaxValue(input);
+            result.ShouldBe(60);
+            return Task.CompletedTask;
+        }
+
+        private int ShowParamsMaxValue(params int[] arr)
+        {
+            int maxValue = 0;
+            if (arr != null && arr.Length > 0)
+            {
+                Array.Sort(arr);
+                maxValue = arr[^1];
+            }
+            return maxValue;
+        }
+        private int ShowMaxValue(int[] arr)
+        {
+            int maxValue = 0;
+            if (arr != null && arr.Length > 0)
+            {
+                Array.Sort(arr);
+                maxValue = arr[^1];
+            }
+            return maxValue;
+        }
+    }
+}
