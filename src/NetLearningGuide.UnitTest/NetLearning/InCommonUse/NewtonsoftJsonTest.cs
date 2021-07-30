4362b2f343bb51151b5a052945dde7b15946ceb2
Vinson <305896628@qq.com>
2021-07-30 00:35:43 -0700

    NewtonsoftJson OptIn Test Case

---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/NewtonsoftJsonTest.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class NewtonsoftJsonTest
+    {
+        //https://www.cnblogs.com/mqxs/p/9646354.html
+        #region MemberSerialization.OptIn
+        [JsonObject(MemberSerialization.OptIn)]
+        public class NewtonsoftJsonOptInModel
+        {
+            public int Age { get; set; }
+            [JsonProperty]
+            public string Name { get; set; }
+            public string Sex { get; set; }
+            public bool IsMarry { get; set; }
+            public DateTime Birthday { get; set; }
+        }
+
+        [Fact]
+        public Task NewtonsoftJsonOptInTestCase1()
+        {
+            var model = new NewtonsoftJsonOptInModel() { Age = 1, Birthday = DateTime.Today, IsMarry = true, Name = "Aaron", Sex = "Men" };
+            var convert = JsonConvert.SerializeObject(model);
+            convert.ShouldContain("Name");
+            convert.ShouldNotContain("Age");
+            convert.ShouldNotContain("Sex");
+            convert.ShouldNotContain("IsMarry");
+            convert.ShouldNotContain("Birthday");
+            return Task.CompletedTask;
+        }
+        #endregion
+    }
+}
