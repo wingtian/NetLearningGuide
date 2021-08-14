15804c05c4dee24e21066c5498fada4534f031b0
Vinson <305896628@qq.com>
2021-08-14 00:34:48 -0700

    New Class test case

---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/NewClassTest.cs
@@ -0,0 +1,41 @@
+﻿using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class NewClassTest
+    {
+        public class NewTemp
+        {
+            public NewTemp(int status)
+            {
+                Status = status;
+            }
+
+            public int Status { get; set; }
+        }
+
+        private NewTemp GetTemp(NewTemp temp)
+        {
+            return temp;
+        }
+        [Fact]
+        public Task NewClassTestCase1()
+        {
+            NewTemp temp = GetTemp(null);
+            try
+            {
+                if (temp.Status == 1) //Reshaper 没提示
+                {
+                }
+            }
+            catch (Exception e)
+            {
+                e.Message.ShouldBe("Object reference not set to an instance of an object.");
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
