5ee6db24c6c2dd257550a3240ad76ad2c72479e2
Vinson <305896628@qq.com>
2021-08-15 15:59:35 -0700

    using test case

---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/UsingTest.cs
@@ -0,0 +1,49 @@
+﻿using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class UsingTest
+    {
+        [Fact]
+        public Task UsingTestCase1()
+        {
+            UsingTestModel test;
+            using (var us = new UsingTestClass())
+            {
+                test = us.Model ;
+            }
+            test.Output.ShouldBe("TEST");
+            return Task.CompletedTask;
+        }
+    }
+    public class UsingTestClass : IDisposable
+    {
+        public UsingTestModel Model;
+
+        public UsingTestClass()
+        {
+            Model = GetModel();
+        }
+
+        private UsingTestModel GetModel()
+        {
+            return new UsingTestModel("TEST");
+        }
+
+        public void Dispose()
+        {
+            Model = null;
+        }
+    }
+
+    public class UsingTestModel
+    {
+        public UsingTestModel(string output)
+        {
+            Output = output;
+        }
+        public string Output { get; set; }
+    }
+}
