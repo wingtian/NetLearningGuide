5223ac92fdbcd95e654fae495bf92a546d20ad6c
Vinson <305896628@qq.com>
2021-08-25 01:02:47 -0700

    隐式转换

---
+++ src/NetLearningGuide.UnitTest/NetLearning/CSharpLeaning/ChapterSix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.CSharpLeaning
+{
+    public class ChapterSix
+    {
+        /// <summary>
+        /// 隐式转换
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public Task ImplicitTestCase1()
+        {
+            var test = (UtmCoordinates)(new GpsCoordinates());
+            test.Test.ShouldBe("HHH");
+            return Task.CompletedTask;
+        }
+
+        class GpsCoordinates
+        { 
+            public static implicit operator UtmCoordinates(GpsCoordinates gps)
+            {
+                return new UtmCoordinates()
+                {
+                    Test = "HHH"
+                };
+            }
+        }
+
+        class UtmCoordinates
+        {
+            public string Test { get; set; }
+        }
+    }
+}
