d996149b7f6ee82d1605b35c1c53f386d3bbfb59
Vinson <305896628@qq.com>
2021-08-27 00:44:41 -0700

    add test case

--- src/NetLearningGuide.UnitTest/NetLearning/CSharpLeaning/ChapterSix.cs
+++ src/NetLearningGuide.UnitTest/NetLearning/CSharpLeaning/ChapterSix.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Shouldly;
+using System;
 using System.Threading.Tasks;
-using Shouldly;
 using Xunit;
 
 namespace NetLearningGuide.UnitTest.NetLearning.CSharpLeaning
 {
     public class ChapterSix
     {
+        #region 隐式转换
         /// <summary>
         /// 隐式转换
         /// </summary>
@@ -17,13 +15,13 @@ namespace NetLearningGuide.UnitTest.NetLearning.CSharpLeaning
         [Fact]
         public Task ImplicitTestCase1()
         {
-            var test = (UtmCoordinates)(new GpsCoordinates());
+            UtmCoordinates test = new GpsCoordinates();
             test.Test.ShouldBe("HHH");
             return Task.CompletedTask;
         }
 
         class GpsCoordinates
-        { 
+        {
             public static implicit operator UtmCoordinates(GpsCoordinates gps)
             {
                 return new UtmCoordinates()
@@ -37,5 +35,55 @@ namespace NetLearningGuide.UnitTest.NetLearning.CSharpLeaning
         {
             public string Test { get; set; }
         }
+        #endregion
+        #region 显式转换
+        /// <summary>
+        /// 显式转换
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public Task ExplicitTestCase1()
+        {
+            var test = (UtmCoordinatesCase2)(new GpsCoordinatesCase2()); //(UtmCoordinatesCase2) 代表显示转换
+            test.Test.ShouldBe("HHH");
+            return Task.CompletedTask;
+        }
+
+        class GpsCoordinatesCase2
+        {
+            public static explicit operator UtmCoordinatesCase2(GpsCoordinatesCase2 gps)
+            {
+                return new UtmCoordinatesCase2()
+                {
+                    Test = "HHH"
+                };
+            }
+        }
+        class UtmCoordinatesCase2
+        {
+            public string Test { get; set; }
+        }
+        #endregion 
+        #region protected 访问修饰符 
+        public class ProtectTest
+        {
+            protected Guid Id { get; set; }
+        }
+
+        public class ContactTest : ProtectTest
+        {
+            void Save()
+            {
+                var test = Id;
+            }
+
+            void Load(ProtectTest test)
+            {
+                // 错误  CS1540 无法通过“ChapterSix.ProtectTest”类型的限定符访问受保护的成员“ChapterSix.ProtectTest.Id”；
+                // 限定符必须是“ChapterSix.ContactTest”类型(或者从该类型派生)
+                //var model = test.Id; 
+            }
+        }
+        #endregion
     }
 }
---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/DateTimeTest.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class DateTimeTest
+    {
+        [Fact]
+        public Task DateTimeSubtractTestCase()
+        {
+            var test1 = DateDiff(Convert.ToDateTime("2021-07-22"), Convert.ToDateTime("2021-07-28"));
+            var test2 = DateDiff(Convert.ToDateTime("2021-07-15"), Convert.ToDateTime("2021-07-28"));
+            test1.ShouldBe(6);
+            test2.ShouldBe(13);
+            return Task.CompletedTask;
+        }
+        public int DateDiff(DateTime dateStart, DateTime dateEnd)
+        {
+            DateTime start = Convert.ToDateTime(dateStart.ToShortDateString());
+            DateTime end = Convert.ToDateTime(dateEnd.ToShortDateString());
+            TimeSpan sp = end.Subtract(start);
+            return sp.Days;
+        }
+    }
+}
