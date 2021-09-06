2f86351a087bc56f51c707e6b325a87a50e34ab4
Vinson <305896628@qq.com>
2021-09-06 00:24:43 -0700

    add enum case

--- src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/EnumTest.cs
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/EnumTest.cs
@@ -83,14 +83,26 @@ namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
             Saturday = 32,
             Sunday = 64,
         }
+        enum WeekEnum
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 4,
+            Thursday = 8,
+            Friday = 16,
+            Saturday = 32,
+            Sunday = 64,
+        }
 
         [Fact]
         public Task EnumFlagsTestCase1()
         {
             int test = (int)(Week.Monday | Week.Tuesday);
             test.ShouldBe(3);
-            var test2 = (Week)Enum.Parse(typeof(Week), "3"); 
+            var test2 = (Week)Enum.Parse(typeof(Week), "3");
             test2.ShouldBe(Week.Monday | Week.Tuesday);
+            var test21 = ((WeekEnum)Enum.Parse(typeof(WeekEnum), "3")).ToString();
+            test21.ShouldBe("3");
             var test3 = (Week)Enum.Parse(typeof(Week), "127");
             test3.ShouldBe(Week.Monday | Week.Tuesday | Week.Wednesday | Week.Thursday | Week.Friday | Week.Saturday | Week.Sunday);
             Week test4 = (Week)Enum.Parse(typeof(Week), "0");
@@ -140,7 +152,7 @@ namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
         }
         [Fact]
         public Task EnumFlagsTestCase4()
-        { 
+        {
             var test1 = (int)Enum.Parse(typeof(Week), "Monday");
             test1.ShouldBe(1);
             return Task.CompletedTask;
