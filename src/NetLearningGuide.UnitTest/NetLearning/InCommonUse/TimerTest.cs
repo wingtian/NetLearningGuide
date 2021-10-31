4e457507ce022912237d08c04ac912312838c728
Vinson <305896628@qq.com>
2021-10-31 15:57:38 -0700

    FromResultTestCase1

---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/TimerTest.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class TimerTest
+    {
+        [Fact]
+        public async Task FromResultTestCase1()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            await Task.Delay(1000);
+            sw.Stop();
+            var time = sw.ElapsedTicks / (decimal)Stopwatch.Frequency;
+            time.ShouldBeGreaterThanOrEqualTo(1);
+        }
+    }
+}
