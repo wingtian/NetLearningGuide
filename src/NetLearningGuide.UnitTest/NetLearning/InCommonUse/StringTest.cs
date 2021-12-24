401ca41cbb0aeca59cc9dfe7a5d0b82ca161c910
Vinson <305896628@qq.com>
2021-12-23 17:10:08 -0800

    TrimStringTest

---
+++ src/NetLearningGuide.UnitTest/NetLearning/InCommonUse/StringTest.cs
@@ -0,0 +1,18 @@
+﻿using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetLearningGuide.UnitTest.NetLearning.InCommonUse
+{
+    public class StringTest
+    {
+        [Fact]
+        public Task TrimStringTest()
+        {
+            string test = "0000123";
+            test = test.TrimStart('0');
+            test.ShouldBe("123");
+            return Task.CompletedTask;
+        }
+    }
+}
